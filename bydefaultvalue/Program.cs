f4ee6e22fd766d92159e6487b8ed63daa6adef11
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:39:19 +0530

    Default

---
+++ bydefaultvalue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace bydefaultvalue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            bool boolVar = true;
+            byte byteVar = default;
+            short shortVar = default;
+            ushort ushortVar = default;
+            int intVar = default;
+            uint uintVar = default;
+            long longVar = default;
+            ulong ulongVar = default;
+            float floatVar = default;
+            double doubleVar = default;
+            char charVar = 'A';
+            Console.WriteLine("Value Data Types ");
+            Console.WriteLine(boolVar);
+            Console.WriteLine(byteVar);
+            Console.WriteLine(shortVar);
+            Console.WriteLine(ushortVar);
+            Console.WriteLine(intVar);
+            Console.WriteLine(uintVar);
+            Console.WriteLine(longVar);
+            Console.WriteLine(ulongVar);
+            Console.WriteLine(floatVar);
+            Console.WriteLine(doubleVar);
+            Console.WriteLine(charVar);
+
+        }
+    }
+}
